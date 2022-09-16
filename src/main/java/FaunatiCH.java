f9745ea01ab455cd8c12039196077d9248a28623
fcardenas07 <f.cardenasreyes.94@gmail.com>
2022-09-16 17:05:05 -0300

    commit 1

--- src/main/java/FaunatiCH.java
+++ src/main/java/FaunatiCH.java
@@ -5,22 +5,13 @@ public class FaunatiCH {
         iniciarJuego();
     }
 
-    public static void iniciarJuego(){
-        System.out.println("BIENVENIDO A FAUNATICH");
-        System.out.println("Este juego te ayudará a aprender sobre la fauna Chilena de una manera muy entretenida.");
+    public static void iniciarJuego() {
+        mostrarTextoDeBienvenida();
         mostrarMenu();
-    }
-
-    private static void mostrarMenu(){
-        System.out.println("""
-                Ingrese una opción:
-                1. Empezar.
-                2. Mostrar estadísticas.
-                3. Salir del juego.""");
         opcionesMenu(ingresar());
     }
 
-    private static void opcionesMenu(int opcionIngresada){
+    private static void opcionesMenu(int opcionIngresada) {
         switch (opcionIngresada) {
             case 1 -> empezar();
             case 2 -> mostrarEstadisticas();
@@ -32,8 +23,8 @@ public class FaunatiCH {
         }
     }
 
-    private static void empezar(){
-        System.out.println("\nA continuación elija la zona geográfica con la que desea empezar:");
+    private static void empezar() {
+        System.out.println("A continuación elija la zona geográfica con la que desea empezar:");
         mostrarMapaGeografico();
         opcionesMapa(ingresar());
 
@@ -46,19 +37,19 @@ public class FaunatiCH {
         //combate(animalUsuario, );
     }
 
-    private static void iniciarCombate(String[][] animalElegidoEnZona,String[][] animalUsuario) {
+    private static void iniciarCombate(String[][] animalElegidoEnZona, String[][] animalUsuario) {
         String[][] animalElegidoEnZonaJugando = animalElegidoEnZona;
         String[][] animalUsuarioJugando = animalUsuario;
 
-        int vidaAnimalElegidoEnZonaJugando =  Integer.parseInt(animalElegidoEnZonaJugando[1][0]);
-        int vidaAnimalUsuarioJugando =  Integer.parseInt(animalUsuarioJugando[1][0]);
+        int vidaAnimalElegidoEnZonaJugando = Integer.parseInt(animalElegidoEnZonaJugando[1][0]);
+        int vidaAnimalUsuarioJugando = Integer.parseInt(animalUsuarioJugando[1][0]);
 
-        while (vidaAnimalUsuarioJugando >=0 || vidaAnimalElegidoEnZonaJugando >= 0){
+        while (vidaAnimalUsuarioJugando >= 0 || vidaAnimalElegidoEnZonaJugando >= 0) {
             mostrarNivelDeVidaAnimales(animalElegidoEnZonaJugando, animalUsuarioJugando);
             jugadaDeUsuario(animalElegidoEnZonaJugando, animalUsuarioJugando); //3 //1 >-2 >-4
-            if (!(vidaAnimalElegidoEnZonaJugando >= 0)){
+            if (!(vidaAnimalElegidoEnZonaJugando >= 0)) {
                 break;
-            }else {
+            } else {
                 jugadaDeCPU(animalElegidoEnZonaJugando, vidaAnimalUsuarioJugando);
             }
         }
@@ -67,10 +58,10 @@ public class FaunatiCH {
     }
 
     private static int jugadaDeCPU(String[][] animalElegidoEnZonaJugando, int vidaAnimalUsuario) {
-        int numeroRandom = (int) ((Math.random()*3)+1);
+        int numeroRandom = (int) ((Math.random() * 3) + 1);
         int ataqueAnimalElegidoEnZonaJugando = Integer.parseInt(animalElegidoEnZonaJugando[3][numeroRandom]);
 
-        return (vidaAnimalUsuario-ataqueAnimalElegidoEnZonaJugando);
+        return (vidaAnimalUsuario - ataqueAnimalElegidoEnZonaJugando);
     }
 
 
@@ -79,45 +70,30 @@ public class FaunatiCH {
     }
 
 
-
     private static void mostrarNivelDeVidaAnimales(String[][] animalElegidoEnZonaJugando, String[][] animalUsuarioJugando) {
         System.out.println("Vida de " + animalElegidoEnZonaJugando[0][0] + ": " + animalElegidoEnZonaJugando[1][0]);
         System.out.println("Vida de " + animalUsuarioJugando[0][0] + ": " + animalUsuarioJugando[1][0]);
     }
 
     private static String[][] generarAnimalUsuario() {
-        String[][] caracteristicasCondor = {{"Condor","Vida","Espupitazo"},{"666","666","666"}};
-        return caracteristicasCondor;
+        return new String[][]{{"Condor", "Vida", "Espupitazo"}, {"666", "666", "666"}};
     }
 
     private static void mostrarCombate(String[][] animalDelUsuario, String[][] animalElegidoEnZona) {
-        System.out.println("El combate será de "+ animalDelUsuario[0][0] + " vs " + animalElegidoEnZona[0][0]);
+        System.out.println("El combate será de " + animalDelUsuario[0][0] + " vs " + animalElegidoEnZona[0][0]);
     }
 
-    private static String[][] opcionesPorZonaElegida(int opcionIngresada){
-        switch (opcionIngresada){
-            case 1:
-                return opcionesAnimalesZonaNorte(opcionIngresada);
-            case 2:
-                return opcionesAnimalesZonaCentral(opcionIngresada);
-
-            case 3:
-                return  opcionesAnimalesZonaSur(opcionIngresada);
-            default:
-                return opcionesPorZonaElegida(ingresar());
-        }
+    private static String[][] opcionesPorZonaElegida(int opcionIngresada) {
+        return switch (opcionIngresada) {
+            case 1 -> opcionesAnimalesZonaNorte(opcionIngresada);
+            case 2 -> opcionesAnimalesZonaCentral(opcionIngresada);
+            case 3 -> opcionesAnimalesZonaSur(opcionIngresada);
+            default -> opcionesPorZonaElegida(ingresar());
+        };
 
     }
 
-    private static void mostrarMapaGeografico(){
-        System.out.println("""
-                A continuación elija la zona geográfica con la que desea empezar:
-                1. Zona norte.
-                2. Zona central.
-                3. Zona sur.""");
-    }
-
-    private static void opcionesMapa(int opcionIngresada){
+    private static void opcionesMapa(int opcionIngresada) {
         switch (opcionIngresada) {
             case 1 -> mostrarAnimalesZonaNorte();
             case 2 -> mostrarAnimalesZonaCentral();
@@ -129,11 +105,11 @@ public class FaunatiCH {
         }
     }
 
-    private static String[][] opcionesAnimalesZonaNorte(int opcionIngresada){
-        String[][] caracteristicasAlpaca = {{"Alpaca"},{"30"},{"Escupitazo","Patada","Mordida"},{"5","3","4"}};
-        String[][] caracteristicasVicuña = {{"Vicuña"},{"30"},{"Mordizco","Patada","Ataque random"},{"5","3","4"}};
-        String[][] caracteristicasGuanaco = {{"Guanaco"},{"30"},{"Escupo","Patada","Ataque random"},{"5","3","4"}};
-        String[][] caracteristicasLlama = {{"Llama"},{"30"},{"Escupo","Silbar","Ataque random"},{"5","3","4"}};
+    private static String[][] opcionesAnimalesZonaNorte(int opcionIngresada) {
+        String[][] caracteristicasAlpaca = {{"Alpaca"}, {"30"}, {"Escupitajo", "Patada", "Mordida"}, {"5", "3", "4"}};
+        String[][] caracteristicasVicuña = {{"Vicuña"}, {"30"}, {"Mordizco", "Patada", "Ataque random"}, {"5", "3", "4"}};
+        String[][] caracteristicasGuanaco = {{"Guanaco"}, {"30"}, {"Escupo", "Patada", "Ataque random"}, {"5", "3", "4"}};
+        String[][] caracteristicasLlama = {{"Llama"}, {"30"}, {"Escupo", "Silbar", "Ataque random"}, {"5", "3", "4"}};
 
         switch (opcionIngresada) {
             case 1:
@@ -153,10 +129,10 @@ public class FaunatiCH {
         return caracteristicasLlama;
     }
 
-    private static String[][] opcionesAnimalesZonaCentral(int opcionIngresada){
-        String[][] caracteristicasZorroCulpeo = {{"Zorro Culpeo"},{"30"},{"Mordedura","Patada","Ataque random"},{"5","3","4"}};
-        String[][] caracteristicasPuma = {{"Puma"},{"30"},{"Mordizco","Embestida","Ataque random"},{"5","3","4"}};
-        String[][] caracteristicasAguilaMora = {{"Águila Mora"},{"30"},{"Garras","En Picada","Ataque random"},{"5","3","4"}};
+    private static String[][] opcionesAnimalesZonaCentral(int opcionIngresada) {
+        String[][] caracteristicasZorroCulpeo = {{"Zorro Culpeo"}, {"30"}, {"Mordedura", "Patada", "Ataque random"}, {"5", "3", "4"}};
+        String[][] caracteristicasPuma = {{"Puma"}, {"30"}, {"Mordizco", "Embestida", "Ataque random"}, {"5", "3", "4"}};
+        String[][] caracteristicasAguilaMora = {{"Águila Mora"}, {"30"}, {"Garras", "En Picada", "Ataque random"}, {"5", "3", "4"}};
 
         switch (opcionIngresada) {
             case 1:
@@ -174,10 +150,10 @@ public class FaunatiCH {
         return caracteristicasZorroCulpeo;
     }
 
-    private static String[][] opcionesAnimalesZonaSur(int opcionIngresada){
-        String[][] caracteristicasPinguino = {{"Pinguino"},{"30"},{"Picar","Aletazo", "Ataque random", "Ataque random"},{"4","5","3"}};
-        String[][] caracteristicasZorroPatagonico = {{"ZorroPatagonico"},{"30"},{"Mordizo", "Ataque random", "Ataque random 2"},{"3","5","6"}};
-        String[][] caracteristicasÑandu = {{"Ñandu"},{"31"},{"Aleteo","Patada", "ataque random"},{"7","4","4"}};
+    private static String[][] opcionesAnimalesZonaSur(int opcionIngresada) {
+        String[][] caracteristicasPinguino = {{"Pinguino"}, {"30"}, {"Picar", "Aletazo", "Ataque random", "Ataque random"}, {"4", "5", "3"}};
+        String[][] caracteristicasZorroPatagonico = {{"ZorroPatagonico"}, {"30"}, {"Mordizo", "Ataque random", "Ataque random 2"}, {"3", "5", "6"}};
+        String[][] caracteristicasÑandu = {{"Ñandu"}, {"31"}, {"Aleteo", "Patada", "ataque random"}, {"7", "4", "4"}};
 
 
         switch (opcionIngresada) {
@@ -224,7 +200,7 @@ public class FaunatiCH {
                 3. Ñandú.""");
     }
 
-    private static void mostrarEstadisticas(){
+    private static void mostrarEstadisticas() {
         System.out.println("""
                 Sus estadísticas de juego son las siguientes:
                 Nivel 1: .....
@@ -232,24 +208,24 @@ public class FaunatiCH {
                 Nivel 3: .....""");
     }
 
-    private static String[][] caracteristicasAnimalesZonaNorte(){
-        String[][] caracteristicasAlpaca = {{"Vida","Espupitazo"},{"30","5","3"}};
+    private static String[][] caracteristicasAnimalesZonaNorte() {
+        String[][] caracteristicasAlpaca = {{"Vida", "Espupitazo"}, {"30", "5", "3"}};
 
-        String[][] caracteristicasVicuña = {{"Vida","Mortizco","Patada"},{"30","5","3"}};
+        String[][] caracteristicasVicuña = {{"Vida", "Mortizco", "Patada"}, {"30", "5", "3"}};
 
-        String[][] caracteristicasGuanaco = {{"Vida", "Escupo", "Patada"},{"30","4","4"}};
+        String[][] caracteristicasGuanaco = {{"Vida", "Escupo", "Patada"}, {"30", "4", "4"}};
 
-        String[][] caracteristicasLlama = {{"Vida","Escupo","Silbar"},{"30","5","5"}};
+        String[][] caracteristicasLlama = {{"Vida", "Escupo", "Silbar"}, {"30", "5", "5"}};
 
         return caracteristicasLlama;
     }
 
-    private static String[][] caracteristicasAnimalInicial(){
-        String[][] caractteristicasQuiltro = {{"Vida","Mordizco","Ladrido"},{"30","6","2"}};
+    private static String[][] caracteristicasAnimalInicial() {
+        String[][] caractteristicasQuiltro = {{"Vida", "Mordizco", "Ladrido"}, {"30", "6", "2"}};
         return caractteristicasQuiltro;
     }
 
-    private static void salirDelJuego(){
+    private static void salirDelJuego() {
         System.out.println("Juego terminado.");
     }
 
@@ -266,5 +242,26 @@ public class FaunatiCH {
             }
         } while (true);
         return entrada;
+    }
+
+    private static void mostrarMapaGeografico() {
+        System.out.println("""
+                A continuación elija la zona geográfica con la que desea empezar:
+                1. Zona norte.
+                2. Zona central.
+                3. Zona sur.""");
+    }
+
+    private static void mostrarMenu() {
+        System.out.println("""
+                Ingrese una opción:
+                1. Empezar.
+                2. Mostrar estadísticas.
+                3. Salir del juego.""");
+    }
+
+    private static void mostrarTextoDeBienvenida() {
+        System.out.println("BIENVENIDO A FAUNATICH");
+        System.out.println("Este juego te ayudará a aprender sobre la fauna Chilena de una manera muy entretenida.");
     }
 }
