bf65e22e5bd7d64d29642e1f3e023061394b5312
marce <m.henriquez11@ufromail.cl>
2022-12-13 23:53:43 -0300

    Se agregan pruebas unitarias

--- src/test/java/FaunatiCHTest.java
+++ src/test/java/FaunatiCHTest.java
@@ -2,7 +2,6 @@ import org.junit.jupiter.api.Test;
 import utils.VerificadorContrasena;
 import utils.VerificadorNombre;
 import utils.VerificadorRut;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
