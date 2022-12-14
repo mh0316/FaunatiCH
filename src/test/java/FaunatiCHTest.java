9a2ce3e06f5d06f4e36bb3cded13c23556986273
marce <m.henriquez11@ufromail.cl>
2022-12-13 23:49:37 -0300

    Se agregan pruebas unitarias

---
+++ src/test/java/FaunatiCHTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test;
+import utils.VerificadorContrasena;
+import utils.VerificadorNombre;
+import utils.VerificadorRut;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FaunatiCHTest {
+
+    @Test
+    public void probarVerificadorDeNombre() {
+        assertTrue(VerificadorNombre.validarNombre("Nico"));  // retorna true
+        assertFalse(VerificadorNombre.validarNombre(" "));  // retorna false
+        assertFalse(VerificadorNombre.validarNombre("Marce lo"));
+        assertTrue(VerificadorNombre.validarNombre("CR7_siuuu"));
+    }
+
+    @Test
+    public void probarVerificadorDeRut() {
+        assertTrue(VerificadorRut.validarRut("208860909"));  // retorna true
+        assertFalse(VerificadorRut.validarRut("2102415077"));  // retorna true
+        assertFalse(VerificadorRut.validarRut("20.886.090-9"));  // retorna true
+        assertFalse(VerificadorRut.validarRut("...."));  // retorna true
+        assertFalse(VerificadorRut.validarRut("----"));  // retorna true
+        assertFalse(VerificadorRut.validarRut("20.886.090-9"));  // va a fallar
+    }
+
+    @Test
+    public void probarVerificadorDeContraseña() {
+        assertFalse(VerificadorContrasena.verificarContraseña("nico12345678"));
+        assertFalse(VerificadorContrasena.verificarContraseña("玩动物群"));
+        assertTrue(VerificadorContrasena.verificarContraseña("marce22"));
+        assertFalse(VerificadorContrasena.verificarContraseña("progra icc"));
+        assertTrue(VerificadorContrasena.verificarContraseña("Mañana14"));
+    }
+}
